165c16aea06c3044a5dc1bec1e418f386e676d11
LaNuggets <lanuggetscuite@gmail.com>
2025-05-16 15:25:33 +0200

    finish

--- getData.py
+++ getData.py
@@ -3,13 +3,13 @@ import pandas as pd
 def chipData(file):
     try:
         df = pd.read_excel(file)
-    except Exception:
-        print(f"Je n'ai pas trouvé le fichier excel. je cherche '{file}' dans le répairtoire où je suis\n")
+    except Exception as e:
+        print(f"Erreur lors de l'ouverture de '{file}' : {e}")
         return None, None, None
     c = df.columns.tolist()
     #columns def
-    firstName = df[c[0]]
-    lastName = df[c[1]]
-    ticketNumber = df[c[2]]
+    firstName = df[c[1]]
+    lastName = df[c[2]]
+    ticket = df[c[3]]
     #
-    return firstName, lastName, ticketNumber
+    return firstName, lastName, ticket
--- main.py
+++ main.py
@@ -1,12 +1,40 @@
 from getData import *
+import re
 
 file = "tombola.xlsx"
 
-firstName, lastName, ticketNumber = chipData(file)
+firstName, lastName, ticket = chipData(file)
 
-participants = []
-for i in range(len(ticketNumber)):
-    participants.append(f"{firstName[i]} {lastName[i]} - Ticket: {ticketNumber[i]}")
+rows = []
+seen_names = set()  # Keep names already seen
 
+for fname, lname, tickets in zip(firstName, lastName, ticket):
+    s = str(tickets)
+    match = re.search(r"\d+", s)
+    try:
+        if match:
+            nb_tickets = int(match.group())
 
-print(participants)
+            full_name = f"{fname.strip().lower()}_{lname.strip().lower()}"
+            for i in range(nb_tickets):
+                # Show number of tickets only for the first occurrence
+                tarif_value = tickets if i == 0 else ""
+                rows.append({
+                    "Prénom": fname,
+                    "Nom": lname,
+                    "Tarif": tarif_value
+                })
+    except ValueError:
+        print(f"Valeur invalide pour le nombre de tickets : {tickets}")
+
+# Create DataFrame
+df = pd.DataFrame(rows)
+
+# Add ticket number
+df["Numéro du ticket"] = range(1, len(df) + 1)
+
+# Organize all column
+df = df[["Numéro du ticket", "Prénom", "Nom", "Tarif"]]
+
+# Save as exel
+df.to_excel("participants.xlsx", index=False)
